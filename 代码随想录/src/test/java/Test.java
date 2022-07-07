ad83125aa577bb8a5a6ba3b8b5b677b240fee9b7
phil <906327724@qq.com>
2022-07-07 21:07:59 +0800

    初次提交

---
+++ 代码随想录/src/test/java/Test.java
@@ -0,0 +1,15 @@
+/**
+ * @Description TODO
+ * @Author Phil
+ * @CreateTime 2022/6/9
+ **/
+
+public class Test {
+
+    public static void main(String[] args) {
+        int n = 5;
+        while (n-- > 1) {
+            System.out.println("第" + n + "次打印");
+        }
+    }
+}
