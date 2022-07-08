d92b2c7bce1aafe70dacd6328de080db40c1974b
phil <906327724@qq.com>
2022-07-08 23:30:36 +0800

    翻转字符串里的单词

---
+++ 代码随想录/src/main/java/string/ReverseWords.java
@@ -0,0 +1,96 @@
+package string;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @Description 翻转字符串里的单词
+ * 给定一个字符串，逐个翻转字符串中的每个单词。
+ * <p>
+ * 示例 1：
+ * 输入: "the sky is blue"
+ * 输出: "blue is sky the"
+ * <p>
+ * 示例 2：
+ * 输入: "  hello world!  "
+ * 输出: "world! hello"
+ * 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * <p>
+ * 示例 3：
+ * 输入: "a good   example"
+ * 输出: "example good a"
+ * 解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ * @Author Phil
+ * @CreateTime 2022/7/8
+ **/
+
+public class ReverseWords {
+
+    public static void main(String[] args) {
+        String reverseWords = new ReverseWords().reverseWords("  hello world! ");
+        System.out.println("reverseWords => " + reverseWords);
+    }
+
+    // jdk自带方法
+    public String reverseWords1(String s) {
+        // 除去开头和末尾的空白字符
+        s = s.trim();
+        // 正则匹配连续的空白字符作为分隔符分割
+        List<String> wordList = Arrays.asList(s.split("\\s+"));
+        Collections.reverse(wordList); //reverse(List<?> list) 可以对List进行逆序排列
+        return String.join(" ", wordList);
+    }
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * 思路：
+     * ①反转字符串  "the sky is blue " => " eulb si yks eht"
+     * ②遍历 " eulb si yks eht"，每次先对某个单词进行反转再移位
+     * 这里以第一个单词进行为演示：" eulb si yks eht" ==反转=> " blue si yks eht" ==移位=> "blue si yks eht"
+     */
+    public String reverseWords(String s) {
+        //步骤1：字符串整体反转（此时其中的单词也都反转了）
+        char[] initialArr = s.toCharArray();
+        reverse(initialArr, 0, s.length() - 1);
+        int k = 0;
+        for (int i = 0; i < initialArr.length; i++) {
+            if (initialArr[i] == ' ') {
+                continue;
+            }
+            int tempCur = i;
+            while (i < initialArr.length && initialArr[i] != ' ') {
+                i++;
+            }
+            for (int j = tempCur; j < i; j++) {
+                if (j == tempCur) { //步骤二：二次反转
+                    reverse(initialArr, tempCur, i - 1);//对指定范围字符串进行反转，不反转从后往前遍历一个个填充有问题
+                }
+                //步骤三：移动操作
+                initialArr[k++] = initialArr[j];
+                if (j == i - 1) { //遍历结束
+                    //避免越界情况，例如=> "asdasd df f"，不加判断最后就会数组越界
+                    if (k < initialArr.length) {
+                        initialArr[k++] = ' ';
+                    }
+                }
+            }
+        }
+        if (k == 0) {
+            return "";
+        } else {
+            //参数三：以防出现如"asdasd df f"=>"f df asdasd"正好凑满不需要省略空格情况
+            return new String(initialArr, 0, (k == initialArr.length) && (initialArr[k - 1] != ' ') ? k : k - 1);
+        }
+    }
+
+    public void reverse(char[] chars, int begin, int end) {
+        for (int i = begin, j = end; i < j; i++, j--) {
+            chars[i] ^= chars[j];
+            chars[j] ^= chars[i];
+            chars[i] ^= chars[j];
+        }
+    }
+
+}
